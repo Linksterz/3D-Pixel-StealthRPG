003fa3a0d50b7e6fced3f7688a35c285f9f15fe3
Linksterz <zengge250@gmail.com>
2024-05-19 14:31:06 -0700

    reworked character movement

--- scripts/globals/game_manager.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var player:Node
-
-func set_player(player_node):
-	player = player_node
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,23 +2,21 @@ extends CharacterBody3D
 
 
 const SPEED = 1.7
-const JUMP_VELOCITY = 4.5
-
-@onready var camera_point = $camera_point
+const JUMP_VELOCITY = 0.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
-@onready var animation_player = $visuals/player/AnimationPlayer
-@onready var visuals = $visuals
+@onready var animation_player = $female/AnimationPlayer
+@onready var visuals = $female
 
 var walking = false
 
 func _ready():
-	GameManager.set_player(self)
-	animation_player.set_blend_time("idle","walk",0.2)
-	animation_player.set_blend_time("walk","idle",0.2)
+	#GameManager.set_player(self)
+	animation_player.set_blend_time("Idle","walk",0.2)
+	animation_player.set_blend_time("walk","Idle",1.0)
 	
 
 func _physics_process(delta):
@@ -32,24 +30,30 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = lerp(velocity.x, direction.x * SPEED, 0.1)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, 0.1)
 		
-		visuals.look_at(direction + position)
+		var rotation_speed := 20.0
+		var weight := 1.0 - pow(0.5, delta * rotation_speed)
+		visuals.rotation.y = lerp_angle(visuals.rotation.y, atan2(-direction.x, -direction.z), weight)
+		
 		
 		if !walking:
 			walking = true
 			animation_player.play("walk")
 		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x, direction.x * SPEED, 0.06)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, 0.06)
+		#print(velocity)
 		
 		if walking:
 			walking = false
-			animation_player.play("idle")
+			animation_player.play("Idle")
 
 	move_and_slide()
